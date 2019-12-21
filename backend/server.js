5bcb29097dafbda7dac823833cafc6ef5ed37eb5
Mia112 <msheriff112@gmail.com>
2019-12-21 18:42:18 -0500

    setup folder and server file

---
+++ backend/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+//create our express server
+const app = express();
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+//require our route files
+const usersRouter = require('./routes/users');
+const videosRouter = require('./routes/videos');
+const AuthRouter = require('./routes/auth');
+
+
+const path = require('path');
+
+//port the server is going to be on
+const PORT = process.env.PORT || 3001;
+
+// Define our middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to the Mongo DB
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true});
+
+const connection = mongoose.connection;
+connection.once('connected', () => {
+  console.log('Database connection successfully');
+});
+connection.on("error", err => {
+  console.log("Mongoose default connection error: " + err);
+});
+
+// Add routes file
+app.use('/users', usersRouter);
+app.use('/videos', videosRouter);
+app.use('/auth', AuthRouter);
+
+// Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static( 'client/build'));
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
+
+
+  
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from "react";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+export class App extends Component {
+  state = {
+    videos: [],
+  };
+
+  videoSearch = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyBMZ252IILNsA0F0h50rbZAIoorxw6cJBQ=0&maxResults=10`
+    );
+    this.setState({ videos: res.data.items});
+  };
+
+  render() {
+    const {videos} = this.state;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
